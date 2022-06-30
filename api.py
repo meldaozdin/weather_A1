dda42ea740af296718ad7d2559bbdb3d6147f0a2
sinanartun <sinanartun@gmail.com>
2022-06-30 20:24:00 +0200

    hello africa

--- api.py
+++ api.py
@@ -4,56 +4,19 @@ import mysql.connector
 import time
 
 
-
-insert_new_message = "INSERT INTO chat (username, message) VALUES (%s, %s);"
-get_messages_sql = 'SELECT * from chat ORDER BY cid ASC;'
-
-actions = ['get_messages', 'send_message']
-
-
-
-
-def re(data):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(str(data))
-    }
+get_data = 'SELECT * from weather ORDER BY id ASC;'
 
 
 def lambda_handler(event, context):
-    time.sleep(3)
-    # TODO implement
-    # print(dir(event))
-    action = 'get_messages'
-    print(os.environ['RDS_DB_NAME'])
-    action = str(event.get('headers').get('action'))
-    if action == 'None':
-        return re('Unable to complete request.')
+    cnx = mysql.connector.connect(host=os.environ['RDS_HOSTNAME'], user=os.environ['RDS_USERNAME'], passwd=os.environ['RDS_PASSWORD'],
+                                  database=os.environ['RDS_DB_NAME'], port=os.environ['RDS_PORT'], charset='utf8')
+    cur = cnx.cursor()
+    cur.execute(get_data)
+    res = cur.fetchall()
 
-    if len(list(filter(lambda x: x == action, actions))) < 1:
-        return re('Unable to complete request: Unknown action. makine soguk')
-
+    cnx.close()
+    ret = []
+    for row in res:
+        ret.append({"id": row[0], "max_temp": row[1], "min_temp": row[2], "avg_temp": row[3], "date": row[4]})
+    return {'statusCode': 200,'headers': {"content-type": "application/json"}, 'body': json.dumps(ret, indent=0, sort_keys=True, default=str)}
     # return controller(event,action)
-    if action == 'get_messages':
-        cnx = mysql.connector.connect(host=os.environ['RDS_HOSTNAME'], user=os.environ['RDS_USERNAME'], passwd=os.environ['RDS_PASSWORD'],
-                                      database=os.environ['RDS_DB_NAME'], port=os.environ['RDS_PORT'], charset='utf8')
-        cur = cnx.cursor()
-        cur.execute(get_messages_sql)
-        res = cur.fetchall()
-
-        cnx.close()
-        ret = []
-        for row in res:
-            ret.append({"cid": row[0], "username": row[1], "message": row[2], "time": row[3]})
-
-        return {'statusCode': 200, 'body': json.dumps(ret, indent=0, sort_keys=True, default=str)}
-    if action == 'send_message':
-        username = str(event.get('headers').get('username'))
-        message = str(event.get('headers').get('message'))
-        cnx = mysql.connector.connect(host=os.environ['RDS_HOSTNAME'], user=os.environ['RDS_USERNAME'], passwd=os.environ['RDS_PASSWORD'],
-                                      database=os.environ['RDS_DB_NAME'], port=os.environ['RDS_PORT'], charset='utf8')
-        cur = cnx.cursor()
-        cur.execute(insert_new_message, (username, message))
-        cnx.commit()
-        cnx.close()
-        return {'statusCode': 200, 'body': json.dumps(cur.lastrowid, indent=0, sort_keys=True, default=str)}
