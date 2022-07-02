82f7f0e73eb99039922382c46b31f5eca48dc58c
Melda <melda@192.168.1.39>
2022-07-02 16:37:57 +0300

    edited.

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ today = date.today()
 yesterday = today - timedelta(days=1)
 
 # Last 7 day (not include today)
-
-URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul/last7days?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp&key=Q4XWJL3XM9K5ANQVCASFC9RC9&contentType=json"
-df = pd.read_json(URL)
-result = pd.DataFrame()
-
-
-for i in range(len(df.days)):
-    data = pd.DataFrame.from_dict(df.days[i])
-    data.drop(['hours'], axis=1, inplace=True)
-    data.drop_duplicates(keep='first', inplace=True)
-    result = result.append(data, ignore_index=True)
-result.to_csv('/tmp/result.csv', index=False)
+#
+# URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul/last7days?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp&key=Q4XWJL3XM9K5ANQVCASFC9RC9&contentType=json"
+# df = pd.read_json(URL)
+# result = pd.DataFrame()
+#
+#
+# for i in range(len(df.days)):
+#     data = pd.DataFrame.from_dict(df.days[i])
+#     data.drop(['hours'], axis=1, inplace=True)
+#     data.drop_duplicates(keep='first', inplace=True)
+#     result = result.append(data, ignore_index=True)
+# result.to_csv('temp/result.csv', index=False)
 
 
 # Today
-result = pd.read_csv('/tmp/result.csv')
+result = pd.read_csv('temp/result.csv')
 
 URL_2 = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul/today?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp&key=Q4XWJL3XM9K5ANQVCASFC9RC9&contentType=json"
 json = requests.get(URL_2).json()
@@ -41,6 +41,6 @@ for i in range(len(df_2.days)):
     data_2.drop_duplicates(keep='first', inplace=True)
     result = result.append(data_2, ignore_index=True)
 result['datetime'] = pd.to_datetime(result['datetime'])
-result.to_csv('/tmp/result.csv', index=False)
+result.to_csv('temp/result.csv', index=False)
 
 
